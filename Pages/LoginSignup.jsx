5291ff0a44e48bec8f6e53dee6b6daad70b5f37d
kananyar <150027067+kananyar@users.noreply.github.com>
2023-11-28 13:21:07 +0530

    Add files via upload

---
+++ Pages/LoginSignup.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './CSS/LoginSignup.css'
+const LoginSignup = () => {
+  return (
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Contine</button>
+        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' is='' />
+          <p>By Continuing, I agree to use the terms of user & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
